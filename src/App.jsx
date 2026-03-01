a18802e4c4032f303cc624fa6c153c9fb673150e
falahadeel-ctrl <falahadeel@gmail.com>
2026-03-01 07:21:50 -0500

    fbervb

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPokemon } from './store/pokemonSlice';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [pokemonName, setPokemonName] = useState('')
+const dispatch= useDispatch();
+const pokemon = useSelector(
+   (state) => 
+    state.pokemon  //result stored in pokemon
+  )
   return (
     <>
-     
+    <h1>title</h1>
+    <input type="text">pokemon name</input>
+    <button>triggers search</button>
     </>
   )
 }
